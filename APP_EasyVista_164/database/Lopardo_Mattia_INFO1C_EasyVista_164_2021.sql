6ae3086e2f7cce004fb8bd32e1f394c88a5e38ff
Mattia <mattialopardo@gmail.com>
2022-06-08 14:34:03 +0200

    commit final

--- APP_EasyVista_164/database/Lopardo_Mattia_INFO1C_EasyVista_164_2021.sql
+++ APP_EasyVista_164/database/Lopardo_Mattia_INFO1C_EasyVista_164_2021.sql
@@ -9,7 +9,7 @@ USE Lopardo_Mattia_INFO1C_EasyVista_164_2021;
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Mer 01 Juin 2022 à 12:30
+-- Généré le :  Mer 08 Juin 2022 à 12:28
 -- Version du serveur :  5.7.11
 -- Version de PHP :  5.6.18
 
@@ -44,7 +44,7 @@ CREATE TABLE `t_adresse` (
 --
 
 INSERT INTO `t_adresse` (`id_adresse`, `nom_adresse`, `NPA_adresse`, `ville_adresse`) VALUES
-(1, 'Via Altisio 36', 1688, 'Sommentier'),
+(1, 'via altisio 36', 1688, 'Sommentier'),
 (2, 'Via Schliffras 98', 5637, 'Geltwil'),
 (3, 'Via Franscini 6', 9327, 'Tübach'),
 (4, 'Via Camischolas sura 4', 3036, 'Detligen'),
@@ -92,12 +92,11 @@ CREATE TABLE `t_demande` (
 --
 
 INSERT INTO `t_demande` (`id_demande`, `nom_demande`, `numero_demande`, `description_demande`) VALUES
-(1, 'nouveau laptop', 9983525, 'Le bénéficiare aimerait un nouveau laptop pour travailler correctement'),
+(1, 'nouvel écran', 9983525, 'Le bénéficiare aimerait un nouveau écran pour travailler correctement'),
 (2, 'nouvelle docking', 9924323, 'Le bénéficiare aimerait une nouvelle docking pour travailler correctement'),
 (3, 'nouvelle souris', 9892444, 'Le bénéficiare aimerait une nouvelle souris pour travailler correctement'),
 (4, 'nouveau clavier', 9821467, 'Le bénéficiare aimerait un nouveau clavier pour travailler correctement'),
-(5, 'nouveau écran', 9977668, 'Le bénéficiare aimerait un nouveau écran pour travailler correctement'),
-(9, 'ef', NULL, NULL);
+(5, 'nouveau écran', 9977668, 'Le bénéficiare aimerait un nouveau écran pour travailler correctement');
 
 -- --------------------------------------------------------
 
@@ -188,20 +187,19 @@ INSERT INTO `t_personne` (`id_personne`, `FK_mail`, `FK_adresse`, `FK_telephone`
 CREATE TABLE `t_pers_attribuer_dem` (
   `id_pers_attribuer_dem` int(11) NOT NULL,
   `FK_personne` int(11) DEFAULT NULL,
-  `FK_demande` int(11) DEFAULT NULL,
-  `date_attribution` date DEFAULT NULL
+  `FK_demande` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `t_pers_attribuer_dem`
 --
 
-INSERT INTO `t_pers_attribuer_dem` (`id_pers_attribuer_dem`, `FK_personne`, `FK_demande`, `date_attribution`) VALUES
-(1, 5, 1, '2022-04-05'),
-(2, 6, 2, '2022-04-05'),
-(3, 7, 3, '2022-04-05'),
-(4, 8, 4, '2022-04-05'),
-(5, 9, 5, '2022-04-05');
+INSERT INTO `t_pers_attribuer_dem` (`id_pers_attribuer_dem`, `FK_personne`, `FK_demande`) VALUES
+(1, 5, 1),
+(2, 6, 2),
+(3, 7, 3),
+(4, 8, 4),
+(5, 9, 5);
 
 -- --------------------------------------------------------
 
@@ -212,17 +210,16 @@ INSERT INTO `t_pers_attribuer_dem` (`id_pers_attribuer_dem`, `FK_personne`, `FK_
 CREATE TABLE `t_pers_attribuer_inc` (
   `id_pers_attribuer_inc` int(11) NOT NULL,
   `FK_personne` int(11) DEFAULT NULL,
-  `Fk_incident` int(11) DEFAULT NULL,
-  `date_attribution` date DEFAULT NULL
+  `Fk_incident` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `t_pers_attribuer_inc`
 --
 
-INSERT INTO `t_pers_attribuer_inc` (`id_pers_attribuer_inc`, `FK_personne`, `Fk_incident`, `date_attribution`) VALUES
-(1, 10, 1, '2022-04-05'),
-(2, 6, 2, '2022-04-05');
+INSERT INTO `t_pers_attribuer_inc` (`id_pers_attribuer_inc`, `FK_personne`, `Fk_incident`) VALUES
+(1, 10, 1),
+(2, 6, 2);
 
 -- --------------------------------------------------------
 
@@ -233,20 +230,19 @@ INSERT INTO `t_pers_attribuer_inc` (`id_pers_attribuer_inc`, `FK_personne`, `Fk_
 CREATE TABLE `t_pers_auteur_dem` (
   `id_pers_auteur_dem` int(11) NOT NULL,
   `FK_personne` int(11) DEFAULT NULL,
-  `FK_demande` int(11) DEFAULT NULL,
-  `date_crea_dem` date DEFAULT NULL
+  `FK_demande` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `t_pers_auteur_dem`
 --
 
-INSERT INTO `t_pers_auteur_dem` (`id_pers_auteur_dem`, `FK_personne`, `FK_demande`, `date_crea_dem`) VALUES
-(1, 1, 1, '2022-04-05'),
-(2, 2, 2, '2022-04-05'),
-(3, 1, 3, '2022-04-05'),
-(4, 2, 4, '2022-04-05'),
-(5, 1, 5, '2022-04-05');
+INSERT INTO `t_pers_auteur_dem` (`id_pers_auteur_dem`, `FK_personne`, `FK_demande`) VALUES
+(1, 1, 1),
+(2, 2, 2),
+(3, 1, 3),
+(4, 2, 4),
+(5, 1, 5);
 
 -- --------------------------------------------------------
 
@@ -257,17 +253,16 @@ INSERT INTO `t_pers_auteur_dem` (`id_pers_auteur_dem`, `FK_personne`, `FK_demand
 CREATE TABLE `t_pers_auteur_inc` (
   `id_pers_auteur_inc` int(11) NOT NULL,
   `FK_personne` int(11) DEFAULT NULL,
-  `FK_incident` int(11) DEFAULT NULL,
-  `date_crea_inc` date DEFAULT NULL
+  `FK_incident` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `t_pers_auteur_inc`
 --
 
-INSERT INTO `t_pers_auteur_inc` (`id_pers_auteur_inc`, `FK_personne`, `FK_incident`, `date_crea_inc`) VALUES
-(1, 3, 1, '2022-04-05'),
-(2, 4, 2, '2022-04-05');
+INSERT INTO `t_pers_auteur_inc` (`id_pers_auteur_inc`, `FK_personne`, `FK_incident`) VALUES
+(1, 3, 1),
+(2, 4, 2);
 
 -- --------------------------------------------------------
 
@@ -286,7 +281,6 @@ CREATE TABLE `t_pers_categorie` (
 --
 
 INSERT INTO `t_pers_categorie` (`id_pers_categorie`, `FK_personne`, `FK_categorie`) VALUES
-(1, 1, 1),
 (2, 2, 1),
 (3, 3, 1),
 (4, 4, 1),
@@ -295,7 +289,8 @@ INSERT INTO `t_pers_categorie` (`id_pers_categorie`, `FK_personne`, `FK_categori
 (7, 7, 2),
 (8, 8, 2),
 (9, 9, 2),
-(10, 10, 2);
+(10, 10, 2),
+(11, 1, 2);
 
 -- --------------------------------------------------------
 
@@ -421,27 +416,27 @@ ALTER TABLE `t_telephone`
 -- AUTO_INCREMENT pour la table `t_adresse`
 --
 ALTER TABLE `t_adresse`
-  MODIFY `id_adresse` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_adresse` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 --
 -- AUTO_INCREMENT pour la table `t_categorie`
 --
 ALTER TABLE `t_categorie`
-  MODIFY `id_categorie` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_categorie` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT pour la table `t_demande`
 --
 ALTER TABLE `t_demande`
-  MODIFY `id_demande` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_demande` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT pour la table `t_incident`
 --
 ALTER TABLE `t_incident`
-  MODIFY `id_incident` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_incident` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT pour la table `t_mail`
 --
 ALTER TABLE `t_mail`
-  MODIFY `id_mail` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_mail` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 --
 -- AUTO_INCREMENT pour la table `t_personne`
 --
@@ -451,32 +446,32 @@ ALTER TABLE `t_personne`
 -- AUTO_INCREMENT pour la table `t_pers_attribuer_dem`
 --
 ALTER TABLE `t_pers_attribuer_dem`
-  MODIFY `id_pers_attribuer_dem` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_pers_attribuer_dem` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT pour la table `t_pers_attribuer_inc`
 --
 ALTER TABLE `t_pers_attribuer_inc`
-  MODIFY `id_pers_attribuer_inc` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_pers_attribuer_inc` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT pour la table `t_pers_auteur_dem`
 --
 ALTER TABLE `t_pers_auteur_dem`
-  MODIFY `id_pers_auteur_dem` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_pers_auteur_dem` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT pour la table `t_pers_auteur_inc`
 --
 ALTER TABLE `t_pers_auteur_inc`
-  MODIFY `id_pers_auteur_inc` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_pers_auteur_inc` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT pour la table `t_pers_categorie`
 --
 ALTER TABLE `t_pers_categorie`
-  MODIFY `id_pers_categorie` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_pers_categorie` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 --
 -- AUTO_INCREMENT pour la table `t_telephone`
 --
 ALTER TABLE `t_telephone`
-  MODIFY `id_telephone` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_telephone` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 --
 -- Contraintes pour les tables exportées
 --
